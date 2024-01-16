e986a22726cb8eecfffef86ccdbf4fe3a97e9a82
Huzaefa1212 <baighuzefa32@gmail.com>
2024-01-16 10:10:50 +0530

    added signup btn

--- app/src/main/java/com/example/ecommrce/Login1Activity.java
+++ app/src/main/java/com/example/ecommrce/Login1Activity.java
@@ -2,13 +2,24 @@ package com.example.ecommrce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login1Activity extends AppCompatActivity {
-    int i=0;
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
+        signup = findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login1Activity.this, Registration1Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ecommrce/MainActivity.java
+++ app/src/main/java/com/example/ecommrce/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.ecommrce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
+        signup = findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Registration1Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
