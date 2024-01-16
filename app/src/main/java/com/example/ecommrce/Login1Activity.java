9b9776f47f8eb27c229a9e254b0d64d5abae98e1
Huzaefa1212 <baighuzefa32@gmail.com>
2024-01-16 09:50:35 +0530

    second

--- app/src/main/java/com/example/ecommrce/Login1Activity.java
+++ app/src/main/java/com/example/ecommrce/Login1Activity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Login1Activity extends AppCompatActivity {
-
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
