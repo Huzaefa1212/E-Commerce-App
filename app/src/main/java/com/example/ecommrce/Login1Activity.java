943e9c48b1c489bfd5ebd46578ed5271f59d67e3
Huzaefa1212 <baighuzefa32@gmail.com>
2024-01-16 09:39:44 +0530

    FIRST

---
+++ app/src/main/java/com/example/ecommrce/Login1Activity.java
@@ -0,0 +1,14 @@
+package com.example.ecommrce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+    }
+}
